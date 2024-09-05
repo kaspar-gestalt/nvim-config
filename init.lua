b17ef577318971d5410cb616c7cf050deff018da
Kaspar Naraghi <naraghi@gestalt-robotics.com>
2024-09-05 12:13:40 +0200

    Add previously configure LSP stuff

--- init.lua
+++ init.lua
@@ -19,3 +19,13 @@ require "user.indentline"
 require "user.alpha"
 require "user.whichkey"
 require "user.autocommands"
+
+--vim.api.nvim_set_keymap('n', '<Up>', '<Nop>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('n', '<Down>', '<Nop>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('n', '<Left>', '<Nop>', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('n', '<Right>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-J>', ':bprev<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-K>', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-F>', [[:lua require('telescope.builtin').live_grep({default_text = vim.fn.expand('<cword>')})<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'pylog', 'logging.debug(f":{}")', { noremap = true, silent = true })
+--vim.api.nvim_set_keymap('i', 'pylog', [[logging.debug(f"<++>: {<++>}")]], { expr = true, noremap = true })
